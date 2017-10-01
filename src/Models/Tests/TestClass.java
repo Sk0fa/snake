ae4218633c1c2970ad1fa667ee73a836b84e3955
Dm17r1y <dmitriy.kopylov2016@gmail.com>
2017-10-01 23:57:24 +0500

    two tests

--- src/Models/Tests/TestClass.java
+++ src/Models/Tests/TestClass.java
@@ -10,11 +10,19 @@ public class TestClass {
     @Test
     public void testSnakeMove() {
         GameMap gameMap = new GameMap(3, 3);
-        Snake snake = new Snake(new Point(0, 0), 1, Direction.Down, gameMap);
-        gameMap.AddSnake(snake);
+        Snake snake = new Snake(new Point(0, 0), 1, Direction.Up, gameMap);
         snake.Move();
-        assertEquals(snake.getHead().getPosition(), new Point(0, 1));
-        assertEquals(snake.getTail().get(0), new Point(0, 0));
+        assertEquals(new Point(0, 1), snake.getHead().getPosition());
+        assertEquals(new Point(0, 0), snake.getTail().get(0).getPosition());
+    }
+
+    @Test
+    public void testCycleMove() {
+        GameMap gameMap = new GameMap(3,3);
+        Snake snake = new Snake(new Point(0, 0), 1, Direction.Right, gameMap);
+        snake.Move();
+        assertEquals(new Point(2, 0), snake.getHead().getPosition());
+        assertEquals(new Point(0, 0), snake.getTail().get(0).getPosition());
     }
 
 }
