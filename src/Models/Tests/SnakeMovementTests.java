a3fda90763844f85f84fbce8e930a7449e9a2832
Dm17r1y <dmitriy.kopylov2016@gmail.com>
2017-10-05 19:04:33 +0500

    Update tests

--- src/Models/Tests/SnakeMovementTests.java
+++ src/Models/Tests/SnakeMovementTests.java
@@ -2,16 +2,40 @@ import Models.Direction;
 import Models.GameMap;
 import Models.Point;
 import Models.Snake;
+import org.junit.Before;
 import org.junit.Test;
 import static junit.framework.Assert.assertEquals;
 
 public class SnakeMovementTests {
 
+    GameMap gameMap;
+    Snake snake;
+
+    @Before
+    public void setUp() {
+        gameMap = new GameMap(3, 3);
+        snake = new Snake(new Point(0, 0), 1, Direction.Up,
+                Direction.Right, gameMap);
+    }
+
+    @Test
+    public void testCorrectInitialization() {
+        assertEquals(snake.getDirection(), Direction.Up);
+        assertEquals(snake.getHead().getPosition(), new Point(0, 0));
+        assertEquals(snake.getTail().toArray().length, 1);
+        assertEquals(snake.getTail().peek().getPosition(), new Point(1, 0));
+    }
+
+    @Test
+    public void testSetDirection() {
+        snake.setDirection(Direction.Left);
+        assertEquals(snake.getDirection(), Direction.Left);
+        snake.setDirection(Direction.Right);
+        assertEquals(snake.getDirection(), Direction.Left);
+    }
+
     @Test
     public void testSnakeMove() {
-        GameMap gameMap = new GameMap(3, 3);
-        Snake snake = new Snake(new Point(0, 0), 1, Direction.Up,
-                Direction.Right, gameMap);
         snake.move();
         assertEquals(new Point(0, 1), snake.getHead().getPosition());
         assertEquals(new Point(0, 0), snake.getTail().peek().getPosition());
@@ -19,9 +43,7 @@ public class SnakeMovementTests {
 
     @Test
     public void testCycleMove() {
-        GameMap gameMap = new GameMap(3,3);
-        Snake snake = new Snake(new Point(0, 0), 1,Direction.Left,
-                Direction.Right, gameMap);
+        snake.setDirection(Direction.Left);
         snake.move();
         assertEquals(new Point(2, 0), snake.getHead().getPosition());
         assertEquals(new Point(0, 0), snake.getTail().peek().getPosition());
