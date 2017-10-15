37456ff49c7918365a6d3215104166e97c10ad90
vladek <vlad.eganov@gmail.com>
2017-10-15 13:51:53 +0500

    Prosit

--- src/Models/Point.java
+++ src/Models/Point.java
@@ -4,9 +4,9 @@ public class Point {
     public int X;
     public int Y;
 
-    public Point(int X, int Y) {
-        this.X = X;
-        this.Y = Y;
+    public Point(int x, int y) {
+        this.X = x;
+        this.Y = y;
     }
 
     @Override
