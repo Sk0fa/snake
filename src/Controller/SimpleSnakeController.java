de7f674c7318920a54e3a6bff24f9d8de2c8d125
FeastInc <feast.inc.original@gmail.com>
2017-10-29 17:13:34 +0500

    add controllers

--- src/Controller/SimpleSnakeController.java
+++ src/Controller/SimpleSnakeController.java
@@ -2,13 +2,19 @@ package Controller;
 
 import Models.Direction;
 import Models.Snake;
+import javafx.scene.input.KeyCode;
+
+import java.awt.event.KeyEvent;
+import java.util.HashMap;
 
 public class SimpleSnakeController implements ISnakeController{
 
     private Snake snake;
+    private HashMap<Integer, Direction> actions;
 
-    public SimpleSnakeController(Snake snake) {
+    public SimpleSnakeController(Snake snake, HashMap<Integer, Direction> actions) {
         this.snake = snake;
+        this.actions = actions;
     }
 
     @Override
@@ -19,5 +25,11 @@ public class SimpleSnakeController implements ISnakeController{
     @Override
     public void SetDirection(Direction direction) {
         snake.setDirection(direction);
+    }
+
+    public void actionPerformed(KeyEvent actionEvent) {
+        if (actions.containsKey(actionEvent.getKeyCode())) {
+            snake.setDirection(actions.get(actionEvent.getKeyCode()));
+        }
     }
 }
--- src/View/Gui.java
+++ src/View/Gui.java
@@ -1,11 +1,19 @@
 package View;
 
+import Controller.SimpleGameController;
+import Controller.SimpleSnakeController;
+import Models.Direction;
 import Models.IGame;
+import Models.Snake;
+import javafx.scene.input.KeyCode;
 
 import javax.swing.*;
 import javax.swing.Timer;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.HashMap;
 
 public class Gui extends JFrame {
 
@@ -14,6 +22,32 @@ public class Gui extends JFrame {
 
     public Gui(IGame game) {
         super("Snake");
+
+        // Контроллер для игры
+        SimpleGameController gameController = new SimpleGameController(game);
+        this.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                gameController.actionPerformed(e);
+            }
+        });
+
+        // Контроллер для змеек
+        for(Snake snake : game.getMap().getSnakes()) {
+            HashMap<Integer, Direction> actions = new HashMap<>();
+            actions.put(KeyEvent.VK_A, Direction.Left);
+            actions.put(KeyEvent.VK_D, Direction.Right);
+            actions.put(KeyEvent.VK_W, Direction.Up);
+            actions.put(KeyEvent.VK_S, Direction.Down);
+            SimpleSnakeController snakeController = new SimpleSnakeController(snake, actions);
+            this.addKeyListener(new KeyAdapter() {
+                @Override
+                public void keyPressed(KeyEvent e) {
+                    snakeController.actionPerformed(e);
+                }
+            });
+        }
+
         this.setBounds(100, 100, CELLSIZE * game.getMap().getWidth(),
                 CELLSIZE * game.getMap().getHeight());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
