aecd61e66970023582698b73bdfc06e6011945d3
FeastInc <feast.inc.original@gmail.com>
2017-11-26 20:00:40 +0500

    Add listener for F5 and F6 for GameController

--- src/Controller/SimpleGameController.java
+++ src/Controller/SimpleGameController.java
@@ -1,18 +1,23 @@
 package Controller;
 
 import Models.IGame;
+import View.Gui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.*;
 
 public class SimpleGameController implements IGameController{
 
+    private static final String FILENAME = "data.dat";
     private IGame game;
+    private Gui gui;
 
-    public SimpleGameController(IGame game) {
+    public SimpleGameController(IGame game, Gui gui) {
         this.game = game;
+        this.gui = gui;
     }
 
     @Override
@@ -21,5 +26,28 @@ public class SimpleGameController implements IGameController{
     }
 
     public void actionPerformed(KeyEvent actionEvent) {
+        switch (actionEvent.getKeyCode()) {
+            case KeyEvent.VK_F5:
+                try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILENAME))) {
+                    oos.writeObject(this.game);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case KeyEvent.VK_F6:
+                try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILENAME))) {
+                    IGame loadGame = (IGame)ois.readObject();
+                    gui.setGame(loadGame);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
     }
 }
